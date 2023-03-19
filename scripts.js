ed69ccb5bfdca6ec78b2a145a2d71410ada28534
gloria nashipae <glorialepilal@gmail.com>
2023-03-19 16:03:57 +0300

    animation

--- scripts.js
+++ scripts.js
@@ -4,14 +4,4 @@ const navlink = document.querySelector('.nav__link');
 hamburger.addEventListener('click', () => (
     navLink.classList.toggle('hide')
 ));
-button.addEventListener('click', () =>{
-    main.style.display ="none";
-    createacct.style.display = "block";
-})
-window.alert("Welcome!")
-window.location = "./signUp.html";
 
-
-$('.toggle-btn').on('click',function() {
-    $(this).toggleClass('active')
-});
